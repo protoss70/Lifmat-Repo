22a2f10b0d37651ae17fbfeb293aca1b128f9370
protoss70 <57136915+protoss70@users.noreply.github.com>
2023-10-24 20:50:33 +0200

    update for the night

--- index.js
+++ index.js
@@ -1,101 +1,116 @@
-nameRoute = 'https://core.alquist.ai/file/manual_storage/faiss_dbs/d728e045e484e4bb01ac73bc32e3c7f0/manual.pdf';
-let loadingTask;
-let pdfDoc = null,
-canvas = document.querySelector('#cnv'),
-ctx = canvas.getContext('2d'),
-scale = 1.5,
-numPage = 1;
 
-const changePDF = (nameRoute, numPage) => {
-    loadingTask = pdfjsLib.getDocument(nameRoute)
-    loadingTask.promise.then(pdfDoc_ => {
-        pdfDoc = pdfDoc_;
-        document.querySelector('#npages').innerHTML = pdfDoc.numPages;
-        GeneratePDF(numPage)
-    });
-}
-
-// changePDF(nameRoute, numPage);
-
-const GeneratePDF = numPage => {
-    pdfDoc.getPage(numPage).then(page => {
-        let viewport = page.getViewport({ scale: scale });
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-        let renderContext = {
-            canvasContext: ctx,
-            viewport: viewport
-        }
-        page.render(renderContext);
-    })
-    document.querySelector('#npages').innerHTML = numPage;
-}
-
-const PrevPage = () => {
-    if (numPage === 1) {
-        return
-    }
-    numPage--;
-    GeneratePDF(numPage);
-}
-
-const NextPage = () => {
-    if (numPage >= pdfDoc.numPages) {
-        return
-    }
-    numPage++;
-    GeneratePDF(numPage);
-}
-
-document.querySelector('#prev').addEventListener('click', PrevPage)
-document.querySelector('#next').addEventListener('click', NextPage)
-
-const container = document.getElementById("cnv"); // Replace with your PDF viewer container.
-
-container.addEventListener("wheel", (event) => {
-    // Calculate the intended page navigation.
-    const isScrollingUp = event.deltaY < 0;
-
-    if (isScrollingUp && numPage > 1) {
-        numPage--; // Scroll up to the previous page.
-    } else if (!isScrollingUp && numPage < pdfDoc.numPages) {
-        numPage++; // Scroll down to the next page.
-    }
-
-    // Perform page navigation using PDF.js.
-    GeneratePDF(numPage);
-
-    // Prevent the default scroll behavior only if navigating pages.
-    if (numPage !== 1 && numPage !== pdfDoc.numPages) {
-        event.preventDefault();
-    }
-});
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-var canvasID ="cnv"
 
 var clientParams = {
-    botKey: "6537b0f7a255e42135762f28",
     allowUrlParams: true,
     fullScreen: false,
     guiMode: 'chat',
     backgroundColor: '#6737ff',
-    textInputEnabled: true,
+    textInputEnabled: false,
+    searchMethods: {text: false, voice: false},
     domain: '.flowstorm.ai',
     mode: 'voice',
-    elementId: "botDiv",
-    collapsable: true,
+    elementId: "bot1",
+    collapsable: false,
     interactionMode: "GUIDE",
-    sound: true,
-    title: "Porsche",
+    sound: false,
+    title: "Tebokan 120 mg, po...",
     search: true,
-    controlIcons: {mic: true, mute: true, restart: true, magnifier: false},
-    canvasID,
-    pdfPageCallback: GeneratePDF,
-    changePDF
+    controlIcons: {mic: false, mute: false, restart: false, magnifier: false},
+    userMessageTextColor: '#297dc7',
+    botMessageBackgroundColor: '#297dc7',
+    userMessageBackgroundColor: '#ffffff',
+    placementMode: "fill"
 };
+
+
 
 const bot1 = new initFSClientBot({
     ...clientParams,
+    botKey: "6537fea9a255e42135781033",
 });
+
+const bot2 = new initFSClientBot({
+    ...clientParams,
+    elementId: "bot2",
+    title: "286 mg/g dentální pasta",
+    botKey: "6537fee9bcfa4117213fa88d"
+});
+
+const bot3 = new initFSClientBot({
+    ...clientParams,
+    elementId: "bot3",
+    title: "Čaj z listu senny...",
+    botKey: "6537fc7ca255e4213577d655"
+});
+
+const bot4 = new initFSClientBot({
+    ...clientParams,
+    elementId: "bot4",
+    title: "Yttrium (90Y) colloid suspen...",
+    botKey: "6537fe67bcfa4117213f9353"
+});
+
+const bot5 = new initFSClientBot({
+    ...clientParams,
+    elementId: "bot5",
+    title: "PANZYNORM forte-N",
+    botKey: "6537ff13bcfa4117213fae05"
+});
+
+const bot6 = new initFSClientBot({
+    ...clientParams,
+    elementId: "bot6",
+    title: "Enhertu 100 mg prášek...",
+    botKey: "6537ff36bcfa4117213fb267"
+});
+
+function returnbArray(){
+    const bot1C = document.getElementById("botC1")
+    const bot2C = document.getElementById("botC2")
+    const bot3C = document.getElementById("botC3")
+    const bot4C = document.getElementById("botC4")
+    const bot5C = document.getElementById("botC5")
+    const bot6C = document.getElementById("botC6")
+    return [bot1C, bot2C, bot3C, bot4C, bot5C, bot6C];
+} 
+
+const bot1C = document.getElementById("bot1")
+const bot2C = document.getElementById("bot2")
+const bot3C = document.getElementById("bot3")
+const bot4C = document.getElementById("bot4")
+const bot5C = document.getElementById("bot5")
+const bot6C = document.getElementById("bot6")
+
+const cArray = [bot1C, bot2C, bot3C, bot4C, bot5C, bot6C]
+const bArray = [bot1, bot2, bot3, bot4, bot5, bot6]
+
+function intro(){
+    document.getElementById("searchBarBarBar").classList.remove("hidden");
+    for (let index = 0; index < bArray.length; index++) {
+        const bot = bArray[index];
+        if (returnbArray()[index].checked){
+            console.log(index);
+            console.log(bot)
+            console.log(bot.parentNode)
+            cArray[index].parentElement.classList.remove("hidden");
+            bot.botUI.startButtonCallback();
+            bot.botUI.chatRestartCallback();
+        }else{
+            cArray[index].parentElement.classList.add("hidden");
+        }
+    }
+}
+
+function askQuestion(){
+    const query = document.getElementById("searcher").value;
+    for (let index = 0; index < bArray.length; index++) {
+        const bot = bArray[index];
+        if (returnbArray()[index].checked){
+            bot.botUI.setUserText(query);
+            bot.bot.handleOnTextInput(query, false, false);
+            bot.botUI.toggleLoading(true);
+        }
+    }
+}
+
+
